1c409a3ee852e336296d358c43837747013dbb9d
SerafimC <henrweb@gmail.com>
2016-03-09 21:46:05 -0300

    Update Lista01_Ex01.c

--- Lista01_Ex01.c
+++ Lista01_Ex01.c
@@ -3,16 +3,24 @@
 
 int main(){
 
-    int controle, i = -1;
-    int *p = (int *) malloc(5*sizeof(int));;
+    int controle, i = 0, max = 5;
+    int *p = (int *) malloc(max*sizeof(int));;
 
     if(p == NULL){
         printf("memória insuficiente\n");
     }
     else{
         do{
-            i++;
             controle = scanf("%d", &p[i]);
+            i++;
+            if(i>=max){
+                max = max + 5;
+                p = (int *) realloc(p, max*sizeof(int));
+
+                if(p == NULL){
+                    printf("memória insuficiente\n");
+                }
+            }
         }while(controle != 0);
     }
 
