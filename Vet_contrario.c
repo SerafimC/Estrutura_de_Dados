5b6e593904be070f8c312205ec181c11bd4c5f48
SerafimC <henrweb@gmail.com>
2016-03-16 21:55:20 -0300

    Update Vet_contrario.c

--- Vet_contrario.c
+++ Vet_contrario.c
@@ -2,27 +2,42 @@
 #include <string.h>
 #include <stdlib.h>
 
-char *push(char *x, char *vet_contrario);
+
+// Funções pop e push
+// POP -  Remoção de dados a partir do topo da fila
+// PUSH - Inclusão de dados a partir do topo da fila
+int push(char *x, int topo);
 void pop(char *x);
 
+//Função principal, na qual exister um vetor de char fixo que será invertido com a implementação de PILHAS
 int main(){
-    char vet[5], *q, vet_contrario[5];
+    char vet[5];
+    int top = 4;
 
     scanf("%s", &vet);
 
-    printf("%s \n aa",  push(vet, vet_contrario));
+    pop(vet);
+    push(vet, top);
+
+    if(top == -1){
+        exit(0);
+    }
+    else{
+        printf("%s\nTopo: %d", vet, top);
+    }
 }
 
-char *push(char *x, char *vet_contrario){
-  
+int push(char *x, int topo){
+    int i;
+    char vet_contrario[5];
 
-    //for(i = 0; i < 4; i++){
-    //        strcpy(vet_contrario[i], x[3-i]);
-    //}
-    vet_contrario[0] = x[0];
-    vet_contrario[1] = x[1];
-    vet_contrario[2] = x[2];
-    vet_contrario[3] = x[3];
-    return vet_contrario;
+    strcpy(vet_contrario, x);
+
+    for(i = (topo); i > -1; i--){
+            x[i] = vet_contrario[5-i];
+            ;
+    }
+
+    return 0;
 
 }
